bcedb8270ed36f6e3d1cd073aafa1478aeda82bb
jboone <jboone@pariserderm.com>
2017-02-15 17:21:35 -0500

    add libs\subscriptions and  tests

--- test/first_spec.js
+++ test/first_spec.js
@@ -1,8 +1,14 @@
 var assert = require('assert');
+
 describe('a feature', function () {
     describe('a scenario', function () {
         it('does somethig specific', function () {
             assert(true);
+        });
+
+        it('also needs to do this', () => {
+            assert(true);
+            require('assert');
         });
     });
 });
