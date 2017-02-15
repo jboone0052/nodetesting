0f2e5258dd3ca212208bb57b80a49eb404355931
jboone <jboone@pariserderm.com>
2017-02-15 16:58:05 -0500

    initial commit

---
+++ test/first_spec.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('a feature', function () {
+    describe('a scenario', function () {
+        it('does somethig specific', function () {
+            assert(true);
+        });
+    });
+});
